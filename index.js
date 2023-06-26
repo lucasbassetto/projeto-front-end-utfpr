4e2fd7be4548420d7cf1cbab7959073d95272fb0
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-06-26 13:59:23 -0300

    Implementando funcao addAndDelete, implementação do botao excluir

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+// ------------------- ADICIONAR POST -----------------------
+ const btnAdicionar = document.querySelector('.btn-dialog-adicionar');
+ const inputTitulo = document.querySelector(".input-titulo");
+ const inputDescricao = document.querySelector(".input-descricao");
+ const inputImagem = document.querySelector(".input-link");
+ const postagens = document.querySelector(".postagens");
+
+ const dialogNovoPost = document.querySelector('.dialog-novo-post');
+
+ //  Box Adicionar Novo Post - Neste comando, ao clicar no botão, abre o Dialog para inserir um Post.
+  const boxNovoPost = document.querySelector('.box-adicionar-post'); 
+ 
+  boxNovoPost.addEventListener('click', function() {
+      dialogNovoPost.className = 'dialog-novo-post-show'; 
+      
+  });
+ 
+  // Botão fechar a janela do Dialog - Neste comando, ao clicar no botão, a janela do Dialog irá se fechar
+  const btnFecharDialog= document.querySelector('.btn-dialog-fechar');
+ 
+  btnFecharDialog.addEventListener('click', function() {
+     dialogNovoPost.className = 'dialog-novo-post';
+  }); 
+
+var postagem = [];
+
+if(localStorage.posts) {
+    postagem = JSON.parse(localStorage.posts)
+    //funcao
+}
+
 function novaPublicacao(id, titulo, descricao, link){
      this.id = id; 
      this.titulo = titulo; 
@@ -5,40 +36,24 @@ function novaPublicacao(id, titulo, descricao, link){
      this.link = link; 
  }
 
- var postagem = [];
 
- const dialogNovoPost = document.querySelector('.dialog-novo-post');
+ class Implementacao {
 
-//  Box Adicionar Novo Post - Neste comando, ao clicar no botão, abre o Dialog para inserir um Post.
- const boxNovoPost = document.querySelector('.box-adicionar-post'); 
+    constructor() {
 
- boxNovoPost.addEventListener('click', function() {
-     dialogNovoPost.className = 'dialog-novo-post-show'; 
-     
- });
+    }
 
- // Botão fechar a janela do Dialog - Neste comando, ao clicar no botão, a janela do Dialog irá se fechar
- const btnFecharDialog= document.querySelector('.btn-dialog-fechar');
+    addAndDelete() {
 
- btnFecharDialog.addEventListener('click', function() {
-    dialogNovoPost.className = 'dialog-novo-post';
- }); 
-
- // ------------------- ADICIONAR POST -----------------------
- const btnAdicionar = document.querySelector('.btn-dialog-adicionar');
- const inputTitulo = document.querySelector(".input-titulo");
- const inputDescricao = document.querySelector(".input-descricao");
- const inputImagem = document.querySelector(".input-link");
- const postagens = document.querySelector(".postagens");
-
- btnAdicionar.addEventListener('click', function() {
-    
     let lista = document.querySelector('.row-post'); 
     let adicionarPublicacao = new novaPublicacao(localStorage.getItem('id'), inputTitulo.value, inputDescricao.value, inputImagem.value);
 
     postagem.push(adicionarPublicacao);
 
+    localStorage.posts = JSON.stringify(postagem);
+
     let post = document.createElement("div"); 
+    let idPost = document.createElement("p"); 
     let tituloPost = document.createElement("h1");
     let descricaoPost = document.createElement("p"); 
     let imagemPost = document.createElement("img"); 
@@ -46,15 +61,16 @@ function novaPublicacao(id, titulo, descricao, link){
     let btnExcluir = document.createElement("button");
 
     post.className = "publicacao"; 
+    idPost.innerHTML = adicionarPublicacao.id;
     tituloPost.innerHTML = adicionarPublicacao.titulo; 
     descricaoPost.innerHTML = adicionarPublicacao.descricao;
-
     btnEditar.innerHTML = "Editar"; 
     btnEditar.className = "btn-editar-post";  
     btnExcluir.innerHTML = "Excluir";  
     btnExcluir.className = "btn-excluir-post";  
     imagemPost.src = adicionarPublicacao.link; 
 
+    post.appendChild(idPost);
     post.appendChild(tituloPost); 
     post.appendChild(descricaoPost); 
     post.appendChild(imagemPost); 
@@ -62,7 +78,19 @@ function novaPublicacao(id, titulo, descricao, link){
     post.appendChild(btnExcluir); 
     lista.appendChild(post)
 
- });
+    btnExcluir.addEventListener('click', function() {
+        lista.removeChild(post)
+    });
+    
+    }
+ }
+
+ var implementacao = new Implementacao()
+
+ btnAdicionar.addEventListener('click', implementacao.addAndDelete)
+
+
+
 
 
  // --------------------- EXCLUIR POST --------------------
